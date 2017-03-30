2e0c23a22d98c3c1295181d6dd49c14a17ba6c1e
Kevin Zhong <jeffrevin@gmail.com>
2017-03-30 20:07:50 +0000

    created functions and secretword

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 //Step 1 TASK: create a variable called secretWord and set it to null
-
+var secretWord = null
 // When this function is called the game is staged to be played.
 function prepareGame() {
 // defines the secret word
   secretWord = ['J','A','V','A','S','C','R', 'I', 'P', 'T'];
 
 // Step 1 TASK: call the drawWord and drawHangman function here (inside the prepareGame function).   
-
+drawWord();
+drawHangman();
 }
 
 // This function draws the correct amount of underscores under hangman depending on the length of the word.
@@ -19,5 +20,6 @@ function drawHangman() {
 
 $(document).ready(function() {
 // Step 1 TASK: call the prepare game function and print the secret word to the console. Then run this program.
-
+prepareGame()
+console.log(secretWord);
 });
