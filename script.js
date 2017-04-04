a3402a37b0ce6f052b164f6b5a859bd884aa6dcc
Kevin Zhong <jeffrevin@gmail.com>
2017-04-04 21:06:04 +0000

    keys recorded

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //Step 1 TASK: create a variable called secretWord and set it to null
-var secretWord = null
+var secretWord = null;
 // When this function is called the game is staged to be played.
 function prepareGame() {
 // defines the secret word
-  secretWord = ['J','A','V','A','S','C','R', 'I', 'P', 'T'];
+  secretWord = ['J','A','V','A','S','C','R','I','P','T'];
 // Step 1 TASK: call the drawWord and drawHangman function here (inside the prepareGame function).   
 drawWord();
 drawHangman();
@@ -11,6 +11,8 @@ drawHangman();
 
 // This function draws the correct amount of underscores under hangman depending on the length of the word.
 function drawWord() {
+    $("#word").empty();
+    
     secretWord.forEach(function() {
         $("#word").append("_");
     });
@@ -22,6 +24,15 @@ function drawHangman() {
 
 $(document).ready(function() {
 // Step 1 TASK: call the prepare game function and print the secret word to the console. Then run this program.
-prepareGame();
-console.log(secretWord);
+  prepareGame();
+  console.log(secretWord);
+  console.log("hello");
+  function onKeyDown(event) {
+    var x = event.key;
+    var y = x.toUpperCase();
+    alert("You clicked " + y);
+    console.log(event);
+  }
+  
+  $("body").keydown(onKeyDown);
 });
