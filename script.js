d90f2b54445e93b0a4e32dec0d02de57cd3e5e93
Kevin Zhong <jeffrevin@gmail.com>
2017-04-06 20:11:09 +0000

    wrong and correct guesses arrays

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 //Step 1 TASK: create a variable called secretWord and set it to null
 var secretWord = null;
+var correctGuesses = [];
+var wrongGuesses = [];
 // When this function is called the game is staged to be played.
 function prepareGame() {
 // defines the secret word
@@ -8,7 +10,6 @@ function prepareGame() {
 drawWord();
 drawHangman();
 }
-
 // This function draws the correct amount of underscores under hangman depending on the length of the word.
 function drawWord() {
     $("#word").empty();
@@ -17,11 +18,10 @@ function drawWord() {
         $("#word").append("_");
     });
 }
-
 // This function draws the hangman images depending on the number of wrong guesses there are.
 function drawHangman() {
+  
 }
-
 $(document).ready(function() {
 // Step 1 TASK: call the prepare game function and print the secret word to the console. Then run this program.
   prepareGame();
@@ -30,15 +30,22 @@ $(document).ready(function() {
   function onKeyPress(event) {
     var x = event.key;
     var y = x.toUpperCase();
+    
+    var correct;
     secretWord.forEach(function(letter, index) {
       if(y === secretWord[index]) {
-        alert("That is the correct letter");
+        //alert("That is the correct letter");
       } else {
-        alert("That is the wrong letter");
+        //alert("That is the wrong letter");
       }
     });
+    if(correct) {
+      correctGuesses.push(y);
+    } else {
+      wrongGuesses.push(y);
+    }
     console.log(event);
   }
-  
   $("body").keypress(onKeyPress);
+  console.log(correctGuesses);
 });
